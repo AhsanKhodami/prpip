7cb6573a4cbd27d84364eec944b3ed540e77cb85
Mohammad ahsan <itseithan89@gmail.com>
2024-11-19 12:02:32 +0100

    bump to vesion 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prpip",
-    version="0.1.0",
+    version="0.1.1",
     author="Your Name",
     author_email="your_email@example.com",
     description="A package for reconstructing pupil size and handling eye-tracker blinks.",
